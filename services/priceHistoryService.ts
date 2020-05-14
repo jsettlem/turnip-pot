8bb89300d181fc6d8b056ad8c273c99b5e966591
jsettlem <pimanrules+github@gmail.com>
2020-05-13 22:57:12 -0400

    fixed db bug

--- services/priceHistoryService.ts
+++ services/priceHistoryService.ts
@@ -5,8 +5,13 @@ import {PriceHistory} from "../models/priceHistory";
 export namespace PriceHistoryService {
 	const users = new keyv();
 
-	export function getPriceHistory(userId: Snowflake): Promise<PriceHistory> {
-		return users.get(userId);
+	export async function getPriceHistory(userId: Snowflake): Promise<PriceHistory> {
+		let history: object = await users.get(userId);
+		if (history !== undefined) {
+			return new PriceHistory(history["currentWeek"], history["previousPattern"], history["prices"])
+		}
+		return undefined
+
 	}
 
 	export function savePriceHistory(userId: Snowflake, priceHistory: PriceHistory): Promise<PriceHistory> {
